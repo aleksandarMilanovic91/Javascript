b75d9a22cd0a37332c36b0ccf85949c90ea30ab6
aleksandar <a.milanovic06@gmail.com>
2019-11-27 14:54:31 +0100

    JS-Uvod,domaci

---
+++ js/main.js
@@ -0,0 +1,33 @@
+//Prvi zadatak//
+
+var Peter,
+	Jim;
+
+	Peter = 3500;
+	Jim = 5000;
+
+	console.log(Peter <= Jim);
+
+//Drugi zadatak//
+
+var John,
+	Mike;
+
+	John = 38;
+	Mike = ((John+8)/2)-8;
+
+	console.log(Mike);	
+
+//Treci zadatak//
+
+var firstFlock,
+	secondFlock;
+
+	firstFlock = 78;
+	secondFlock = 57;
+
+	firstFlock = firstFlock - 12;
+	secondFlock = (secondFlock+12) - 4;
+
+	console.log(firstFlock >= secondFlock);
+
